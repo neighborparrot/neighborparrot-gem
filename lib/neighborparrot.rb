406464d8c30527984cbe71fbc99af795add7fe81
Eloy Gomez <eloy@indeos.es>
2011-12-31 20:56:13 +0100

    Check parameters before send

--- lib/neighborparrot.rb
+++ lib/neighborparrot.rb
@@ -1,15 +1,32 @@
 class Neighborparrot
   require 'net/http'
   require 'uri'
-  attr_reader :POST_URL
-  EVENTS_URL = 'http://localhost:9000/open'
+  require 'pp'
   POST_URL = URI.parse('http://localhost:9000/post')
 
   # EVENTS_URL = 'http://neighborparrot.net/open'
   # POST_URL = 'http://neighborparrot.net/post'
 
-  def self.post(channel, data)
-    params = { :channel => channel, :data => data }
-    postData = Net::HTTP.post_form(POST_URL, params)
+  # Create a new instance of the client
+  # @param [String] key: The key assigned to your account
+  # in neighborparrot.com site
+  def initialize(key)
+    raise "Invalid key" if key.nil? || key.length == 0
+    @key = key
+  end
+
+  # Post a message to a channel
+  # Raise exception if channel is not setted
+  # If empty data, refuse to send nothing
+  # Raise exception if error
+  # @param [String] channel: The channel name
+  # @param [String] string to send
+  def post(channel, data)
+    raise "Channel can't be nil" if channel.nil? || channel.length == 0
+    return false if data.nil? || data.length == 0
+    params = { :key => @key, :channel => channel, :data => data }
+    res = Net::HTTP.post_form(POST_URL, params)
+    raise "Error when post to the neighborparrot: #{res.value}" unless res.nil? || res.is_a?(Net::HTTPSuccess)
+    return true
   end
 end
--- spec/neighborparrot_spec.rb
+++ spec/neighborparrot_spec.rb
@@ -1,10 +1,51 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Neighborparrot" do
+  before :each do
+    @key = 'test_key'
+    @parrot = Neighborparrot.new(@key)
+  end
+
+  it "should rails exception when instantiate without key" do
+    expect { Neighborparrot.new() }.to raise_error
+  end
+
+  it "should rails exception when nil key" do
+    expect { Neighborparrot.new(nil) }.to raise_error
+  end
+
+  it "should rails exception when empty key" do
+    expect { Neighborparrot.new('') }.to raise_error
+  end
+
   it "should send post request with valid parameters" do
-    expec_params = { :channel => 'test', :data => 'test string' }
+    expec_params = { :key => @key, :channel => 'test', :data => 'test string' }
     url =  URI.parse('http://localhost:9000/post')
     Net::HTTP.should_receive(:post_form).with(url, expec_params)
-    Neighborparrot.post(expec_params[:channel], expec_params[:data])
+    @parrot.post(expec_params[:channel], expec_params[:data])
   end
+
+  it "should raise exception with nill channel" do
+    expect { @parrot.post(nil, 'test string') }.to raise_error
+  end
+
+  it "should raise exception with empty channel" do
+    expect { @parrot.post('', 'test string') }.to raise_error
+  end
+
+  it "should not send message with nil data" do
+    Net::HTTP.should_not_receive(:post_form)
+    @parrot.post('test-channel', nil)
+  end
+
+  it "should not send message with empty data" do
+    Net::HTTP.should_not_receive(:post_form)
+    @parrot.post('test-channel', '')
+  end
+
+  it "should raise exception if can't send the request" do
+    Net::HTTP.should_not_receive(:post_form)
+    @parrot.post('test-channel', '')
+  end
+
 end
