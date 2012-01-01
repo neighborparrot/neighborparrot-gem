b3e093422920c5baec22552a106afb5e939a435e
Eloy Gomez <eloy@indeos.es>
2012-01-01 23:29:22 +0100

    Add open method for client mode

--- lib/neighborparrot.rb
+++ lib/neighborparrot.rb
@@ -2,7 +2,10 @@ class Neighborparrot
   require 'net/http'
   require 'uri'
   require 'pp'
-  POST_URL = URI.parse('http://localhost:9000/post')
+  NEIGHBOR_PROTOCOL = "http"
+  NEIGHBOR_HOST = "localhost"
+  NEIGHBOR_PORT = 9000
+  POST_URL = URI.parse("#{NEIGHBOR_PROTOCOL}://#{NEIGHBOR_HOST}:#{NEIGHBOR_PORT}/post")
 
   # EVENTS_URL = 'http://neighborparrot.net/open'
   # POST_URL = 'http://neighborparrot.net/post'
@@ -28,5 +31,87 @@ class Neighborparrot
     res = Net::HTTP.post_form(POST_URL, params)
     raise "Error when post to the neighborparrot: #{res.value}" unless res.nil? || res.is_a?(Net::HTTPSuccess)
     return true
+  end
+
+  # Open a persistent connection to the Neighbor in a new
+  # thread and return true if all works  unless :foreground
+  # options is true.
+  # Current options to the connectio are:
+  # :foreground [Boolean] run the connection in the foreground
+  # stoping the clode flow until the connection is closed by server or
+  # another thread call close
+  #
+  # @param [String] channel to connect
+  # @param [Hash] Options to the connection
+  def open(channel, options={})
+    return false if connected?
+
+    if ! options[:foreground] == true
+      close if @current_thread # If previus thread but closed connection kill it
+      @current_thread = Thread.new(channel, options) do | channel, options|
+        open_connection channel, options
+      end
+      return true
+    else
+      open_connection channel, options
+    end
+  end
+
+  # @return true if a connection exists and is started
+  def connected?
+    @connection && @connection.started?
+  end
+
+  # close and active connection
+  def close
+    return unless connected?
+    @connection.finish()
+    @current_thread.kill
+    @current_thread = nil
+  end
+
+  # To be extended.
+  # Callen when a message is received
+  def on_message(message)
+    puts "received: #{message}."
+  end
+
+  def on_error(error=nil)
+    puts "on error #{error}"
+  end
+
+  def on_close
+    puts "on close"
+  end
+
+  def on_connect
+    puts "connected"
+  end
+
+  def set_connection(connection)
+    @connection = connection
+    on_connect
+  end
+
+  def open_connection(channel, options={})
+    begin
+      Net::HTTP.start(NEIGHBOR_HOST, NEIGHBOR_PORT) do |http|
+        http.read_timeout = 9999999999999999 # TODO Fix this
+        request = Net::HTTP::Get.new "/open?channel=#{channel}"
+        set_connection http
+        http.request request do |response|
+          response.read_body do |chunk|
+            if chunk.start_with? "data:"
+              data = chunk[5..-3] # Remove data: and \n\n
+              on_message data
+            end
+          end
+        end
+      end
+    rescue
+      on_error $!
+      return
+    end
+    on_close
   end
 end
--- spec/neighborparrot_spec.rb
+++ spec/neighborparrot_spec.rb
@@ -6,46 +6,95 @@ describe "Neighborparrot" do
     @parrot = Neighborparrot.new(@key)
   end
 
-  it "should rails exception when instantiate without key" do
-    expect { Neighborparrot.new() }.to raise_error
+  describe "Neigborparrot#post" do
+    it "should rails exception when instantiate without key" do
+      expect { Neighborparrot.new() }.to raise_error
+    end
+
+    it "should rails exception when nil key" do
+      expect { Neighborparrot.new(nil) }.to raise_error
+    end
+
+    it "should rails exception when empty key" do
+      expect { Neighborparrot.new('') }.to raise_error
+    end
+
+    it "should send post request with valid parameters" do
+      expec_params = { :key => @key, :channel => 'test', :data => 'test string' }
+      url =  URI.parse('http://localhost:9000/post')
+      Net::HTTP.should_receive(:post_form).with(url, expec_params)
+      @parrot.post(expec_params[:channel], expec_params[:data])
+    end
+
+    it "should raise exception with nill channel" do
+      expect { @parrot.post(nil, 'test string') }.to raise_error
+    end
+
+    it "should raise exception with empty channel" do
+      expect { @parrot.post('', 'test string') }.to raise_error
+    end
+
+    it "should not send message with nil data" do
+      Net::HTTP.should_not_receive(:post_form)
+      @parrot.post('test-channel', nil)
+    end
+
+    it "should not send message with empty data" do
+      Net::HTTP.should_not_receive(:post_form)
+      @parrot.post('test-channel', '')
+    end
+
+    # TODO
+    it "should raise exception if can't send the request"
+
   end
 
-  it "should rails exception when nil key" do
-    expect { Neighborparrot.new(nil) }.to raise_error
+  describe "Neigborparrot#open" do
+    after :each do
+      @parrot.close
+    end
+
+    it "should open a connection with correct values" do
+      @parrot.should_receive(:on_connect)
+      @parrot.open('test')
+      sleep(2)
+    end
+
+    it "should receive messages" do
+      @parrot.open('test')
+      sleep(1)
+      @parrot.should_receive(:on_message)
+      @parrot.post('test', 'message')
+      sleep(1)
+    end
+
+    it "should return false if already a connection active" do
+      @parrot.open('test')
+      sleep(2)
+      @parrot.open('other test').should be_false
+    end
   end
 
-  it "should rails exception when empty key" do
-    expect { Neighborparrot.new('') }.to raise_error
+  describe "Neigborparrot#close" do
+    it "should close a connection" do
+      @parrot.open('test')
+      sleep(2)
+      @parrot.close()
+      @parrot.connected?.should be_false
+    end
   end
 
-  it "should send post request with valid parameters" do
-    expec_params = { :key => @key, :channel => 'test', :data => 'test string' }
-    url =  URI.parse('http://localhost:9000/post')
-    Net::HTTP.should_receive(:post_form).with(url, expec_params)
-    @parrot.post(expec_params[:channel], expec_params[:data])
-  end
+  describe "Neigborparrot#connected?" do
+    it "should be false before connected" do
+      @parrot.connected?.should be_false
+      @parrot.close
+    end
 
-  it "should raise exception with nill channel" do
-    expect { @parrot.post(nil, 'test string') }.to raise_error
+    it "should be true when connected" do
+      @parrot.open('test')
+      sleep(2)
+      @parrot.connected?.should be_true
+      @parrot.close
+    end
   end
-
-  it "should raise exception with empty channel" do
-    expect { @parrot.post('', 'test string') }.to raise_error
-  end
-
-  it "should not send message with nil data" do
-    Net::HTTP.should_not_receive(:post_form)
-    @parrot.post('test-channel', nil)
-  end
-
-  it "should not send message with empty data" do
-    Net::HTTP.should_not_receive(:post_form)
-    @parrot.post('test-channel', '')
-  end
-
-  it "should raise exception if can't send the request" do
-    Net::HTTP.should_not_receive(:post_form)
-    @parrot.post('test-channel', '')
-  end
-
 end
